6584ee49335c8fca068b48d4d81d3756e01267c9
Zeeshan Ibrahim <72156748+CodeZee7@users.noreply.github.com>
2023-10-24 14:39:00 +0100

    Create insertion-sort.py

---
+++ insertion-sort.py
@@ -0,0 +1,12 @@
+def insertionSort(a_list):
+  print(f"Original list: {a_list}")
+  for i in range(1, len(a_list)):
+    while a_list[i] < a_list[i-1] and i > 0:
+      a_list[i], a_list[i-1] = a_list[i-1], a_list[i]
+      a -= 1
+  print(f"Sorted list: {a_list}")
+
+
+## Example use
+original_list = [3, 5, 1, 7, 2, 8, 6]
+insertionSort(original_list)
